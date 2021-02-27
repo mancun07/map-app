aae9ac8f639ed7f06d1ad5828454dee6b8b6f226
Dima <address>
2021-02-27 10:21:54 +0300

    X

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div className="header">
-            <h1>Learn main facts about chosen country (just by one click!)</h1>
+            <h1>Узнай основные факты о стране (всего одним кликом!)</h1>
         </div>
     )
 }
