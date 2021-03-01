1914738c01762904112fb992a1e65998428bfbee
Dima <address>
2021-03-01 18:21:43 +0300

    X

--- src/components/ExtraInfo.js
+++ src/components/ExtraInfo.js
@@ -20,7 +20,7 @@ const ExtraInfo = ({current}) => {
             <h3 style={{textAlign: 'center'}}>Основная информация:</h3>
             <img style={{width: '100px', display: 'block', margin: 'auto'}} src={current.flag} alt="Flag of the country"/>
             <ul>
-                <li><strong>Название страны: {current.name}</strong></li>
+                <li><strong>Наименование страны: {current.name}</strong></li>
                 <li><strong>Население: {numberWithSpaces(current.population)} </strong></li>
                 <li><strong>Столица: {current.capital}</strong></li>
                 <li><strong style={{color:'yellow'}}>Официальные языки страны:</strong><br/> {current.languages.map((lang, i) => {
@@ -31,7 +31,7 @@ const ExtraInfo = ({current}) => {
                     return <div key={i}>{currency.name}{' '}{currency.symbol ? `(${currency.symbol})` : false}</div>
                 })}</li>
                 <li><strong style={{color:'yellow'}}>Входит в экономические блоки:</strong> {current.regionalBlocs.length ? current.regionalBlocs.map((bloc,i) => {
-                    return <div key={i}>{bloc.name}</div>
+                    return <span key={i}>{bloc.name}{i === (current.regionalBlocs.length - 1) ? '' : ', '}</span>
                 }) : '---'}
                 </li>
                 <li><strong>Местное время:</strong> {current.timezones.map((time, i) => {
